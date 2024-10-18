06791e2adcf1cae93588ff94869505d50daa9f66
Greyback <brow123raul@gmail.com>
2024-10-17 22:05:51 -0500

    tercer commit, trabajando con Git y GitHubClassroom

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -2,7 +2,7 @@ package proyecto01;
 public class Proyecto01 {
 
     public static void main(String[] args) {
-        System.out.println("Manejado desde GitHubVersionado a través de GitCreado por Raúl Andino");
+        System.out.println("Manejado desde GitHubVersionado a través de Git Creado por Raúl Andino");
     }
     
 }
---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,7 @@
+package proyecto02;
+public class Proyecto02 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de JavaVersionado a través de GitManejado desde GitHubAdemás se usar GitHubClassroom");
+    }
+    
+}
